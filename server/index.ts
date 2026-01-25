e64ca521dcccf919ca32fe6edf8fe08547639111
skvoze <kinovalovvasili04@gmail.com>
2026-01-25 20:52:10 +0300

    fix: bind to 0.0.0.0 for deployment

--- server/index.ts
+++ server/index.ts
@@ -99,15 +99,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "127.0.0.1",
-      //reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  const PORT = Number(process.env.PORT) || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  log(`serving on port ${PORT}`);
+});
 })();
