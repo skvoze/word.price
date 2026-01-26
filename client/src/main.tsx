06d683cfe93012dc1d31cde156714a6486fb3f14
skvoze <kinovalovvasili04@gmail.com>
2026-01-26 21:25:30 +0300

    another fix: tg-id and card input

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,14 @@
 alert("Приложение запускается!"); // Это выскочит сразу при открытии
 console.log("Лог из main.tsx");
+const tg = (window as any).Telegram?.WebApp;
+console.log("=== ПРОВЕРКА TG ПРИ СТАРТЕ ===");
+console.log("Весь объект WebApp:", tg);
+console.log("Данные пользователя:", tg?.initDataUnsafe?.user);
+
+if (tg) {
+    tg.ready(); // Сообщаем Телеграму, что приложение готово
+    tg.expand(); // Разворачиваем на весь экран для удобства
+}
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
