c66f965689aac1debf4e324cff2655ac77c1c327
skvoze <kinovalovvasili04@gmail.com>
2026-03-19 21:41:08 +0300

    I tired

--- shared/contracts.ts
+++ shared/contracts.ts
@@ -1,5 +1,5 @@
 export const VAULT_ADDRESS = "0x09AC8b9A30f3b16cfC1228c98a51384F4218353a";
-export const USDC_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e"; // Base Sepolia USDC
+export const USDC_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
 
 export const VAULT_ABI = [
   { name: "deposit", type: "function", stateMutability: "nonpayable", inputs: [{ name: "amount", type: "uint256" }] },
@@ -12,5 +12,6 @@ export const VAULT_ABI = [
 
 export const USDC_ABI = [
   { name: "approve", type: "function", stateMutability: "nonpayable", inputs: [{ name: "spender", type: "address" }, { name: "amount", type: "uint256" }], outputs: [{ name: "", type: "bool" }] },
-  { name: "allowance", type: "function", stateMutability: "view", inputs: [{ name: "owner", type: "address" }, { name: "spender", type: "address" }], outputs: [{ name: "", type: "uint256" }] }
+  { name: "allowance", type: "function", stateMutability: "view", inputs: [{ name: "owner", type: "address" }, { name: "spender", type: "address" }], outputs: [{ name: "", type: "uint256" }] },
+  { name: "balanceOf", type: "function", stateMutability: "view", inputs: [{ name: "account", type: "address" }], outputs: [{ name: "", type: "uint256" }] }
 ] as const;
