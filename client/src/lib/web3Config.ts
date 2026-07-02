7754c69dc23bfac2a266a1503960a32b55ead891
skvoze <kinovalovvasili04@gmail.com>
2026-07-02 14:09:30 +0300

    Another few things

--- client/src/lib/web3Config.ts
+++ client/src/lib/web3Config.ts
@@ -37,7 +37,7 @@ export const config = getDefaultConfig({
   chains: [base, arcTestnet], 
   ssr: true, 
   transports: {
-    [base.id]: http(import.meta.env.RPC_URL ||'https://rpc.ankr.com/base'),
+    [base.id]: http(process.env.RPC_URL||'https://rpc.ankr.com/base'),
     [arcTestnet.id]: http(), 
   },
 });
